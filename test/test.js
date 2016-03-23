ad4f4b33095c39a5c4e6cb218cddfa03ac727739
Eric Conlon <ejconlon@gmail.com>
2016-03-23 13:46:09 -0700

    adding simple tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,23 @@
 const lib = require('../src/lib.js');
 
 module.exports = {
-    test1: function (test) {
-        test.equals(1, 1);
-        test.done();
-    }
+  testParseArgs: function(test) {
+    const input = ['--foo', 'bar', '--baz', 'quux', '--', '--beep', 'boop'];
+    const actual = lib.parseArgs(input);
+    const expected = [{'foo': 'bar', 'baz': 'quux'}, ['--beep', 'boop']];
+    test.deepEqual(actual, expected);
+    test.done();
+  },
+  testLookupArg: function(test) {
+    const input = ['--beep', 'boop'];
+    test.deepEqual(lib.lookupArg(input, '--beep'), 'boop');
+    test.deepEqual(lib.lookupArg(input, '--whatever'), undefined);
+    test.done();
+  },
+  testLookupAllArgs: function(test) {
+    const input = ['--beep', 'boop', '--meep', 'moop', '--beep', 'bop'];
+    test.deepEqual(lib.lookupAllArgs(input, '--beep'), ['boop', 'bop']);
+    test.deepEqual(lib.lookupAllArgs(input, '--whatever'), []);
+    test.done();
+  }
 };
